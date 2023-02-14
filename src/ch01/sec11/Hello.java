26d8fb98268b8b838d6fb574a3c70643809be62e
pcc0105@gmail.com <pcc0105@gmail.com>
2023-02-14 10:54:39 +0900

    first commit

---
+++ src/ch01/sec11/Hello.java
@@ -0,0 +1,18 @@
+package ch01.sec11;
+/**
+ * @atuhor 춘핑이
+ */
+/*
+장제목 : 1장 자바시작하기
+작성일 : 2023.02.14
+*/
+
+public class Hello 	{
+	//프로그램 실행 진입점
+	public static void main(String[] args) {
+		//콘솔에 출력하는 실행문
+		//단축키 : 컨트롤 + /
+		System.out.println("Hello, Java");
+	}
+
+}
