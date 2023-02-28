12152c6567b7e03eb3943e2fdf92395996c5eeed
pcc0105@gmail.com <pcc0105@gmail.com>
2023-02-28 14:00:45 +0900

    ERROR

--- src/ch05/lecture/p08regex/C06Regex.java
+++
@@ -1,9 +0,0 @@
-package ch05.lecture.p08regex;
-
-public class C06Regex {
-	public static void main(String[] args) {
-		System.out.println("Az".matches("[A-Z][a-z]")); //true
-		
-		System.out.println("zA".matches("[A-Z][a-z]")); //false
-	}
-}
