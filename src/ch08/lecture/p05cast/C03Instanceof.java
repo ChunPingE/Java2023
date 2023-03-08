3ba9360c42a3daf541658dc3e76b9beccab37bd1
pcc0105@gmail.com <pcc0105@gmail.com>
2023-03-08 11:56:47 +0900

    29일차

---
+++ src/ch08/lecture/p05cast/C03Instanceof.java
@@ -0,0 +1,11 @@
+package ch08.lecture.p05cast;
+
+public class C03Instanceof {
+	public static void main(String[] args) {
+		MyInterface02 o1 = new MyClass021();
+		MyInterface02 o2 = new MyClass022();
+		
+		
+		
+	}
+}
