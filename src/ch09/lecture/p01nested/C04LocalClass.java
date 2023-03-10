213d3fa192bfd36964e252c93d755398ad082970
pcc0105@gmail.com <pcc0105@gmail.com>
2023-03-10 15:33:14 +0900

    31일차

---
+++ src/ch09/lecture/p01nested/C04LocalClass.java
@@ -0,0 +1,16 @@
+package ch09.lecture.p01nested;
+
+public class C04LocalClass {
+	void method1() {
+		//로컬클래스(local class)
+		//선언된 메소드 내에서만 사용가능
+		class LocalClass1{
+		}
+		LocalClass1 o1 = new LocalClass1();
+	}
+	
+	void method2() {
+		//LocalClass1 o1 = new LocalClass1(); 불가능
+	}
+}
+
---
+++ src/ch09/sec04/exam01/A.java
@@ -0,0 +1,24 @@
+package ch09.sec04.exam01;
+
+public class A {
+	//각 B객체는 이름이 우연히 같을 뿐 다른 클래스이다.
+	
+	//생성자
+	A(){
+		//로컬클래스 선언
+		class B {}
+		
+		//로컬객체 생성
+		B b = new B();
+	}
+	
+	//메소드
+	void method() {
+		//로컬클래스 선언
+		class B {}
+		
+		//로컬객체 생성
+		B b = new B();
+	}
+
+}
