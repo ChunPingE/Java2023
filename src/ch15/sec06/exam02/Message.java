0f34deb650c65e9ac10624bafde4ba86d8c0bc86
pcc0105@gmail.com <pcc0105@gmail.com>
2023-03-16 13:47:06 +0900

    35일차

---
+++ src/ch15/sec06/exam02/Message.java
@@ -0,0 +1,11 @@
+package ch15.sec06.exam02;
+
+public class Message {
+	public String commamd;
+	public String to;
+	
+	public Message(String commamd, String to) {
+		this.commamd = commamd;
+		this.to = to;
+	}
+}
